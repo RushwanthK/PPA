68f75c261e2b8adf41258f33527355498a7d6bba
RushwanthK <rushwanth13@gmail.com>
2025-01-26 13:27:04 +0530

    Have created db models and frontend react website with just page names displaying

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("config.Config")
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    CORS(app)
+    
+    # Register Blueprint
+    from .routes import routes
+    app.register_blueprint(routes)
+
+    # Create database tables
+    with app.app_context():
+        db.create_all()
+
+    return app
---
+++ portfolio-app/src/pages/Assets.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Assets() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Your Assets</h1>
+      <p>Here you can manage your Assets</p>
+    </div>
+  );
+}
