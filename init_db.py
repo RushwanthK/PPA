697dc846a66238ec1b26eef2fa54e684223fb625
RushwanthK <rushwanth13@gmail.com>
2025-06-29 20:15:17 +0530

    created tables in postgres

---
+++ init_db.py
@@ -0,0 +1,11 @@
+# init_db.py
+"""
+from app import create_app, db
+from app.models import *
+
+app = create_app()
+
+with app.app_context():
+    db.create_all()
+    print("✅ Tables created.")
+"""
